7a2918c8b3f14d285bba73ce85ce6fba3653f0d9
Brian Keane <lonesomewhistle@gmail.com>
2014-03-28 10:19:44 -0500

    Built hash methods

--- my_hash/my_hash.rb
+++ my_hash/my_hash.rb
@@ -1,13 +1,19 @@
 class MyHash
   def initialize
+    @array = Array.new(64)
   end
 
   def find_index(key)
-  end
-
-  def []=(key, value)
+    return (key.hash % 63)
   end
 
   def [](key)
+    return @array[self.find_index(key)]
   end
+
+  def []=(key, value)
+    @array[self.find_index(key)] = value
+  end
+
+
 end
